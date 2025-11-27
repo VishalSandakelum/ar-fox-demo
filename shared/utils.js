43c1000d3cf695acd10794f051ec3c3d6ef573c3
Vishal Sandakelum <125721998+VishalSandakelum@users.noreply.github.com>
2025-11-27 15:16:27 +0530

    Change GLTF model URL in gltfLoader

--- shared/utils.js
+++ shared/utils.js
@@ -60,7 +60,7 @@ class Reticle extends THREE.Object3D {
 );*/
 
 window.gltfLoader.load(
-  "https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf",
+  "assets/dragon_flying.glb",
   function (gltf) {
     const flower = gltf.scene.children.find((c) => c.name === "sunflower");
     flower.castShadow = true;
